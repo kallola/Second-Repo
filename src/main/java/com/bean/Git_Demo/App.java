47d06d4632bcf996aff690b2dc110da5d90a9f63
kallola <kallola.mohanty@gmail.com>
2020-03-31 20:50:32 +0530

    First change

---
+++ src/main/java/com/bean/Git_Demo/App.java
@@ -0,0 +1,17 @@
+package com.bean.Git_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public void m1()
+    {
+    	System.out.println("git Demo............");
+    }
+}
